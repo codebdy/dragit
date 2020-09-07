97d05c48ab557b5b923d58be9a06aa5eb7878ce2
Water.Li <lizongbin@hotmail.com>
2020-09-07 17:27:39 +0800

    add

--- src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
+++ src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
@@ -5,6 +5,7 @@ import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import CloseIcon from '@material-ui/icons/Close';
+import TableColumnsList from './TableColumnsList';
 
 
 
@@ -95,22 +96,10 @@ export default function TableColumnsDialog(props:InputProps){
       <ThemeProvider theme={theme}>
         <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
           <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-            Modal title
+            表格列编辑器
           </DialogTitle>
           <DialogContent dividers>
-              <Typography gutterBottom>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              </Typography>
-              <Typography gutterBottom>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-                lacus vel augue laoreet rutrum faucibus dolor auctor.
-              </Typography>
-              <Typography gutterBottom>
-                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-                auctor fringilla.
-              </Typography>
+              <TableColumnsList />
             </DialogContent>
           <DialogActions>
               <Button autoFocus onClick={handleClose} color="primary">
