99ad64d137cfaaa2322b094765537479dc919fe7
Water.Li <lizongbin@hotmail.com>
2020-09-07 17:14:37 +0800

    change theme to light type

--- src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
+++ src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
@@ -1,16 +1,72 @@
 import React, { Fragment } from 'react';
-import { makeStyles, Theme, createStyles, Button, IconButton} from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Button, IconButton, WithStyles, withStyles, Typography, Dialog, responsiveFontSizes, createMuiTheme, ThemeProvider} from '@material-ui/core';
 import { InputProps } from './InputProps';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles((theme: Theme) =>
+
+
+const styles = (theme: Theme) =>
   createStyles({
     more:{
       width:'24px',
 
-    }
+    },
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
 
-  }),
-);
+  const useStyles = makeStyles(styles);
+
+export interface DialogTitleProps extends WithStyles<typeof styles> {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const DialogTitle = withStyles(styles)((props: DialogTitleProps) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const theme = responsiveFontSizes(createMuiTheme({
+  palette: {
+    type: 'light',
+  },
+
+}));
 
 export default function TableColumnsDialog(props:InputProps){
   const classes = useStyles();
@@ -23,10 +79,48 @@ export default function TableColumnsDialog(props:InputProps){
     onChange(field, newValue ||' ');
   };  
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Fragment>
-      <IconButton size="small" className = {classes.more}> ··· </IconButton>
+
+      <Fragment>
+        <IconButton size="small" className = {classes.more} onClick={handleClickOpen}> ··· </IconButton>
+    <ThemeProvider theme={theme}>
+          <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Modal title
+          </DialogTitle>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </Typography>
+            <Typography gutterBottom>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+              lacus vel augue laoreet rutrum faucibus dolor auctor.
+            </Typography>
+            <Typography gutterBottom>
+              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+              scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+              auctor fringilla.
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose} color="primary">
+              Save changes
+            </Button>
+          </DialogActions>
+        </Dialog>      
+    </ThemeProvider>      
     </Fragment>
+
     
   )
 }
