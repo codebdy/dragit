68bea13e2fe6a696949e99393eb7850550385bc3
Water.Li <lizongbin@hotmail.com>
2020-09-07 17:16:19 +0800

    Update TableColumnsDialog.tsx

--- src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
+++ src/designer/Attrebutebox/Inputs/TableColumnsDialog.tsx
@@ -90,35 +90,35 @@ export default function TableColumnsDialog(props:InputProps){
 
   return (
 
-      <Fragment>
-        <IconButton size="small" className = {classes.more} onClick={handleClickOpen}> ··· </IconButton>
-    <ThemeProvider theme={theme}>
-          <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+    <Fragment>
+      <IconButton size="small" className = {classes.more} onClick={handleClickOpen}> ··· </IconButton>
+      <ThemeProvider theme={theme}>
+        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
           <DialogTitle id="customized-dialog-title" onClose={handleClose}>
             Modal title
           </DialogTitle>
           <DialogContent dividers>
-            <Typography gutterBottom>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            </Typography>
-            <Typography gutterBottom>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor.
-            </Typography>
-            <Typography gutterBottom>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-              scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-              auctor fringilla.
-            </Typography>
-          </DialogContent>
+              <Typography gutterBottom>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+              </Typography>
+              <Typography gutterBottom>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor.
+              </Typography>
+              <Typography gutterBottom>
+                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+                auctor fringilla.
+              </Typography>
+            </DialogContent>
           <DialogActions>
-            <Button autoFocus onClick={handleClose} color="primary">
-              Save changes
-            </Button>
-          </DialogActions>
+              <Button autoFocus onClick={handleClose} color="primary">
+                Save changes
+              </Button>
+            </DialogActions>
         </Dialog>      
-    </ThemeProvider>      
+      </ThemeProvider>      
     </Fragment>
 
     
