efbd2bdcb59023157ed6e91133b26e4eb419e99d
Water.Li <lizongbin@hotmail.com>
2020-10-27 15:42:07 +0800

    fixed bug

--- src/admin/views/Medias/MediaGridListFolder.tsx
+++ src/admin/views/Medias/MediaGridListFolder.tsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, IconButton } from '@material-ui/core';
 import MdiIcon from 'components/common/MdiIcon';
 import MediaGridListItemTitle from './MediaGridListItemTitle';
 import { FolderNode } from './MediaFolder';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,16 +24,55 @@ const useStyles = makeStyles((theme: Theme) =>
       color:"#757575",
     },
 
+    mask:{
+      position:'absolute',
+      height:'100%',
+      width:"100%",
+      left:"0",
+      top:"0",
+      background:"rgba(50,50,50, 0.3)",
+      borderRadius:"5px",
+      display:'flex',
+      flexFlow:"column"
+    },
+    toolbar:{
+      height:'40px',
+      width:"100%",
+      display:'flex',
+      flexFlow:'row',
+      justifyContent:'flex-end',
+      padding:'2px',
+    }
+
   }),
 );
 
 export default function MediaGridListFolder(props:{folder:FolderNode, onSelect:(nodeId:string)=>void}){
   const {folder, onSelect} = props;
   const classes = useStyles();
+  const [hover, setHover] = React.useState(false);
+
   return (
     <Fragment>
-      <div className={classes.folder} onDoubleClick = {()=>onSelect(folder.id)}>
+      <div 
+        className={classes.folder} 
+        onDoubleClick = {()=>onSelect(folder.id)}
+        onMouseOver = {()=>setHover(true)}
+        onMouseLeave = {()=>setHover(false)}
+      >
         <MdiIcon className={classes.folderIcon} iconClass = "mdi-folder-outline" size="50" />
+        {
+          hover&&
+          <div className={classes.mask}>
+            <div className={classes.toolbar}>
+              <div>
+                <IconButton size = "small">
+                  <EditIcon fontSize="small" />
+                </IconButton>
+              </div>
+            </div>
+          </div>
+        }
       </div>
       {
         //<LinearProgress />
