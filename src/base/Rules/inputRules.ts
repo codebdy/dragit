f94e31554fdf8d9ab95c26b265919f99321a77d9
Water.Li <lizongbin@hotmail.com>
2020-11-15 12:59:15 +0800

    Refactor TexBox rule

---
+++ src/base/Rules/inputRules.ts
@@ -0,0 +1,40 @@
+import { IProp } from "base/IProp";
+import BooleanInput from "base/PropsInputs/BooleanInput";
+import OptionSelect from "base/PropsInputs/OptionSelect";
+import StringInput from "base/PropsInputs/StringInput";
+
+const inputRules:Array<IProp> =[
+  {
+    name:'variant',
+    label:'variant',
+    input:OptionSelect,
+    props:{
+      items:[
+        {
+          value:'filled',
+          label:'Filled'
+        },
+        {
+          value:'outlined',
+          label:'Outlined'
+        },
+        {
+          value:'standard',
+          label:'Standard'
+        },
+      ]
+    },
+  },
+  {
+    name:'label',
+    label:'label',
+    input:StringInput,
+  }, 
+  {
+    name:'required',
+    label:'required',
+    input:BooleanInput,
+  },
+]
+
+export default inputRules;
--- src/components/TextBox/Rule.ts
+++ src/components/TextBox/Rule.ts
@@ -1,3 +1,9 @@
+import { IProp } from "base/IProp";
+import BooleanInput from "base/PropsInputs/BooleanInput";
+import NumberInput from "base/PropsInputs/NumberInput";
+import OptionSelect from "base/PropsInputs/OptionSelect";
+import StringInput from "base/PropsInputs/StringInput";
+import inputRules from "base/Rules/inputRules";
 import { Rule } from "../../base/Rules/Rule";
 import { INode } from "../../designer/Core/Node/INode";
 
@@ -9,4 +15,47 @@ export class TextBoxRule extends Rule{
   accept(child:INode){
     return false;
   }
+
+  getFields(): Array<IProp>{
+    return [
+      ...inputRules,
+      {
+        name:'size',
+        label:'size',
+        input:OptionSelect,
+        props:{
+          items:[
+            {
+              value:'medium',
+              label:'Medium'
+            },
+            {
+              value:'small',
+              label:'Small'
+            },
+          ]
+        },
+      },
+      {
+        name:'multiline',
+        label:'multiline',
+        input:BooleanInput,
+      },
+      {
+        name:'rows',
+        label:'rows',
+        input:NumberInput,
+        props:{
+          min:1,
+        }
+      },
+      {
+        name:'helperText',
+        label:'helper-text',
+        xs:12,
+        input:StringInput,
+      }
+    ]
+  }
+
 }
