9abac67722a804aceae805f414b830519c5a2df3
Water.Li <lizongbin@hotmail.com>
2020-11-16 07:51:42 +0800

    add validation alert

--- src/admin/views/Page/PageView.tsx
+++ src/admin/views/Page/PageView.tsx
@@ -5,11 +5,13 @@ import { RootState } from "store";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkPageSchema } from "store/page/thunks";
 import { withRouter } from 'react-router-dom';
-import { Container } from "@material-ui/core";
+import { Container, Dialog } from "@material-ui/core";
 import PageSkeleton from "./PageSkeleton";
 import { GO_BACK_ACTION, JUMP_TO_PAGE_ACTION, PageAction, PageJumper } from './PageAction';
 
 import { FormProvider, useForm } from "react-hook-form";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import intl from "react-intl-universal";
 
 const PageView = (props:{match: any, history:any })=>{
   const{history} = props;
@@ -27,8 +29,12 @@ const PageView = (props:{match: any, history:any })=>{
   },[dispatch, moduleId, pageId, dataId]);
   
   const methods = useForm({mode: 'all'});
-  const {handleSubmit} = methods;
+  const {handleSubmit, errors} = methods;
   const onSubmit = (data: any) => console.log('数据提交',data);
+  const onValidate = ()=>{
+    setSubmitted(true);
+  }
+  const [submitted, setSubmitted] = React.useState(false);
   
   const resolvePageUrl=(page:PageJumper)=>{
     return `/admin/module/${page.moduleId}/${page.pageId}` + (page.dataId ? '/' + page.dataId : '' );
@@ -51,6 +57,12 @@ const PageView = (props:{match: any, history:any })=>{
     }
     
   }
+
+  const handleCloseAlert = ()=>{
+    setSubmitted(false);
+  }
+
+  const openAlert = Object.keys(errors).length > 0 && submitted;
 
   const pageContent =  pageInStore.schemaLoading ?
       <PageSkeleton />
@@ -76,12 +88,25 @@ const PageView = (props:{match: any, history:any })=>{
         {
           pageInStore.pageJson?.settings?.isFormPage 
           ?
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit, onValidate)}>
               {pageContent}
             </form>
           :
             pageContent
         }
+
+      <Dialog
+        open={openAlert}
+        onClose={handleCloseAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <Alert severity="error"  onClose={handleCloseAlert}>
+          <AlertTitle>{intl.get('error')}</AlertTitle>
+          {intl.get('input-error')} — <strong>{intl.get('please-confirm')}</strong>
+        </Alert>      
+      </Dialog>
+
       </FormProvider>    
     </Container>
   )
