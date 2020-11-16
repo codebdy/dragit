0b2a1a06eb0ea75ac453e80ebf204e80974c0472
Water.Li <lizongbin@hotmail.com>
2020-11-16 14:34:49 +0800

    Update index.tsx

--- src/designer/ModuleManager/index.tsx
+++ src/designer/ModuleManager/index.tsx
@@ -1,15 +1,29 @@
+import { Button } from "@material-ui/core";
+import Spacer from "components/common/Spacer";
 import DesignerLayout from "designer/Layout";
-import React from "react";
+import React, { Fragment } from "react";
 import LeftContent from "./LeftContent";
+import intl from "react-intl-universal";
+import { withRouter } from 'react-router-dom';
 
-export default function ModuleManager(props:{children?: any}) {
+const ModuleManager = (props:{children?: any, history:any})=>{
+  const{history} = props;
+  const handleReturn = ()=>{
+    history.goBack();
+  }
+
   return (
     <DesignerLayout
       leftArea={
         <LeftContent />
       }
       toolbar = {
-        'xx'
+        <Fragment>
+          <Spacer></Spacer>
+          <Button variant="contained" color="primary" onClick={handleReturn}>
+            {intl.get('go-back')}
+          </Button>          
+        </Fragment>
       }      
     >
       模块管理
@@ -17,3 +31,5 @@ export default function ModuleManager(props:{children?: any}) {
     </DesignerLayout>
   )
 }
+
+export default  withRouter(ModuleManager)
