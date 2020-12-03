e27edfea56f292043692aa3e8214d586a1f31cd5
Water.Li <lizongbin@hotmail.com>
2020-12-03 21:03:01 +0800

    Update Rule.ts

--- src/components/Tree/TreeSelect/Rule.ts
+++ src/components/Tree/TreeSelect/Rule.ts
@@ -1,10 +1,6 @@
 import { Rule } from "base/Rules/Rule";
 import { INode } from "designer/PageEditor/Core/Node/INode";
 import { IProp } from "base/Model/IProp";
-import SwitchInput from "base/PropsInputs/BooleanInput";
-import StringInput from "base/PropsInputs/StringInput";
-import elevationRules from "base/Rules/elevationRules";
-import marginRules from "base/Rules/marginRules";
 
 export class TreeSelectRule extends Rule{
 
