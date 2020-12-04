8dd2237f6ff9b6c363bde179e5ac68dfb0fa1a52
Water.Li <lizongbin@hotmail.com>
2020-12-04 22:10:12 +0800

    Update ToolsAccordion.tsx

--- src/designer/DrawerEditor/ToolsAccordion.tsx
+++ src/designer/DrawerEditor/ToolsAccordion.tsx
@@ -13,11 +13,15 @@ import { ItemMeta } from 'designer/Common/EditableList';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import IMenuItem from 'base/Model/IMenuItem';
 import { RXNode } from 'base/RXNode/RXNode';
+import Scrollbar from 'admin/common/Scrollbar';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '300px',
+      height:'calc(100vh - 100px)',
+      display:'flex',
+      flexFlow:'column',
     },
     list:{
       width:'100%',
@@ -81,89 +85,67 @@ export default function ToolsAccordion(
 
   return (
     <div className={classes.root}>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography>{intl.get('assist-items')}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <List className={classes.list}>
-            {
-              assistItems.map((item, index)=>{
-                return (
-                  <ListItem 
-                    key={index} 
-                    draggable = {true}  
-                    className={classes.item}
-                    onDragStart = {()=>handleDragStart(item.meta as any)}
-                    onDragEnd = {onEndDragNode}
-                  >
-                    <ListItemText primary={item.label} />
-                  </ListItem>
-                )
-              })
-            }
-          </List>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography >{intl.get('customized-modules')}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <List className={classes.list}>
-            {
-              customizedModules?.map(module=>{
-                const meta:IMenuItem ={
-                  type:'item',
-                  title:module.title,
-                  icon:"mdi-circle-small",
-                  to:`/admin/module/${module.id}/`,
-                }
-                return (
-                  <ListItem key={module.id} draggable = {true}  className={classes.item}
-                    onDragStart = {()=>handleDragStart(meta)}
-                    onDragEnd = {onEndDragNode}
-                  >
-                    <ListItemText primary={module.title} />
-                  </ListItem>                  
-                )
-              })
-            }
-          </List>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography >{intl.get('preorder-modules')}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <List className={classes.list}>
-            <ListItem className={classes.item} draggable = {true} 
-              onDragStart = {()=>handleDragStart({
-                type: 'item',
-                title: intl.get('medias'),
-                to: '/admin/medias',
-                icon: 'mdi-image-auto-adjust',
-              })}
-              onDragEnd = {onEndDragNode}
-            >
-              <ListItemText primary={intl.get('medias')} />
-            </ListItem>
-          </List>
-        </AccordionDetails>
-      </Accordion>
+      <Scrollbar>
+        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography>{intl.get('assist-items')}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List className={classes.list}>
+              {
+                assistItems.map((item, index)=>{
+                  return (
+                    <ListItem 
+                      key={index} 
+                      draggable = {true}  
+                      className={classes.item}
+                      onDragStart = {()=>handleDragStart(item.meta as any)}
+                      onDragEnd = {onEndDragNode}
+                    >
+                      <ListItemText primary={item.label} />
+                    </ListItem>
+                  )
+                })
+              }
+            </List>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography >{intl.get('customized-modules')}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List className={classes.list}>
+              {
+                customizedModules?.map(module=>{
+                  const meta:IMenuItem ={
+                    type:'item',
+                    title:module.title,
+                    icon:"mdi-circle-small",
+                    to:`/admin/module/${module.id}/`,
+                  }
+                  return (
+                    <ListItem key={module.id} draggable = {true}  className={classes.item}
+                      onDragStart = {()=>handleDragStart(meta)}
+                      onDragEnd = {onEndDragNode}
+                    >
+                      <ListItemText primary={module.title} />
+                    </ListItem>                  
+                  )
+                })
+              }
+            </List>
+          </AccordionDetails>
+        </Accordion>
+      </Scrollbar>
     </div>
   );
 }
