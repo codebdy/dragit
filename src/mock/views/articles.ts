e6ec2ec6d68a24f3e4d22a41ad7861d899d67e58
Water.Li <lizongbin@hotmail.com>
2020-12-11 16:04:17 +0800

    Update articles.ts

--- src/mock/views/articles.ts
+++ src/mock/views/articles.ts
@@ -2,15 +2,6 @@ import artilceList from "designer/PageEditor/Toolbox/metas/aritcle/listPage"
 
 export default {
   layout:[
-    {
-      name: 'GridRow',
-      props: {
-          justify: 'space-between',
-          alignItems: "center",
-          spacing: 1,
-          marginTop: 2,
-      },
-      children: [artilceList]
-    },
+    artilceList,
   ] 
 }
