9eabc7a34ed1cfbdfd6927dc79795d4f7c7e7d5e
Water.Li <lizongbin@hotmail.com>
2020-12-12 13:42:08 +0800

    Update getModelName.js

--- src/mock/utils/getModelName.js
+++ src/mock/utils/getModelName.js
@@ -2,7 +2,7 @@ import getQueryVariable from 'mock/utils/getQueryVariable';
 
 export function getModelName(url) {
   let modelName = getQueryVariable('modelName', url);
-  console.log(modelName, url);
-  modelName = modelName?.replaceAll('%2F', '/');
+  modelName = modelName?.replace(/%2F/g, '/');
+  console.log(modelName)
   return modelName;
 }
