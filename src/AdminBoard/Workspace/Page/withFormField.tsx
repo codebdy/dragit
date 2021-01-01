74cb18dc3290f2a7bc90ced79632e2e2882247e4
Water.Li <lizongbin@hotmail.com>
2021-01-01 09:30:03 +0800

    remove validation when onBlur

--- src/AdminBoard/Workspace/Page/withFormField.tsx
+++ src/AdminBoard/Workspace/Page/withFormField.tsx
@@ -5,40 +5,9 @@ import { ModelFieldStore } from "./Store/ModelFieldStore";
 
 const withFormField = (Component:any)=>{
   const WithFormField = observer((props:any)=>{
-
     const {field, onlyShow, forwardedRef, empertyValue, rule, helperText, ...rest} = props;
-
-    //const fieldName = useFieldName(field);
-    //const subModelContext = useContext(SubModelContext);
     const fieldStore = useFieldStore(field);
     const modelStore =  useModelStore();
-    //const [value, loading] = useFieldValue(field);    
-    //const [inputValue, setInputValue] = React.useState(value);
-    //const fieldError = useFieldError(fieldName);
-    //const [error, setError] = React.useState(fieldError && fieldError.message);
-    //const registerRule = rule && metaRuleToRegisterRules(rule);
-
-    //useEffect(()=>{
-    //  register(fieldName, registerRule)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    //},[]);
-
-    //useEffect(()=>{
-    //  setError(fieldError);
-    //},[fieldError]);
-
-    //针对1对1面板
-    //useEffect(()=>{
-    //  if(subModelContext.parentField && !subModelContext.model){
-        //setValue(subModelContext.parentField, {});
-    //  }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    //},[field, subModelContext.parentField, subModelContext.model]);
-
-    //useEffect(()=>{
-    //  setInputValue(value);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    //}, [value])
 
     const handleChange = (e:any) => {
       let newValue = e?.target?.value;
@@ -48,21 +17,23 @@ const withFormField = (Component:any)=>{
       }
     }
 
-    const handleBlur = (event:any)=>{
-      event?.stopPropagation();
-      fieldStore?.validate();
-    }
+    /*const handleBlur = (event:any)=>{
+      if(event){
+        event.stopPropagation();
+        fieldStore?.validate();        
+      }
+    }*/
+
     const error = fieldStore?.error;
     const compent = <Component
       ref={forwardedRef}
-      //name = {field}
       loading={fieldStore?.loading || modelStore.loading}
       value={fieldStore?.value || empertyValue || ''}
       {...rest}
       error={!! error}
       helperText = {error || helperText}
       onChange={handleChange}
-      onBlur={handleBlur}
+      //onBlur={handleBlur}
     />;
     return (
       fieldStore instanceof ModelFieldStore?
