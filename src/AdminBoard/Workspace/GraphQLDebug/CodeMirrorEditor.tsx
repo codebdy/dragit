832bd04855a9f336ea99cee4b1f2339e99c8caf4
Water.Li <lizongbin@hotmail.com>
2021-01-07 20:43:14 +0800

    add error area

--- src/AdminBoard/Workspace/GraphQLDebug/CodeMirrorEditor.tsx
+++ src/AdminBoard/Workspace/GraphQLDebug/CodeMirrorEditor.tsx
@@ -10,6 +10,8 @@ import 'codemirror/addon/hint/show-hint';
 import 'codemirror-graphql/hint';
 import 'codemirror-graphql/lint';
 import 'codemirror-graphql/mode';
+import "codemirror/addon/lint/json-lint";
+
 const jsonlint = require("jsonlint-mod");
 
 declare var window:any;
--- src/AdminBoard/Workspace/GraphQLDebug/QueriesDebug.tsx
+++ src/AdminBoard/Workspace/GraphQLDebug/QueriesDebug.tsx
@@ -5,12 +5,14 @@ import {observer} from 'mobx-react-lite';
 import {GQLList} from './GQLList';
 import { print, parse } from 'graphql';
 import {CodeMirrorEditor} from './CodeMirrorEditor';
+import intl from 'react-intl-universal';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flex:1,
       display:'flex',
+      
     },
     container:{
       flex:1,
@@ -24,8 +26,26 @@ const useStyles = makeStyles((theme: Theme) =>
     titleText:{
       padding:theme.spacing(0,2,1,2),
       fontSize:'16px',
+    },
+    error:{
+      flex:1,
+      color:'red',
+      width:'100%',
+      display:'flex',
+      alignItems:'stretch',
+      paddingLeft:'15px',
+    },
+    errorInner:{
+      flex:1,
+      background:"#181818",
+      padding:'2px',
+      wordWrap: 'break-word',
+      whiteSpace :'normal',
+    },
+    pre:{
+      whiteSpace:'pre-wrap',
+      wordWrap:'break-word',
     }
-
   }),
 );
 
@@ -36,9 +56,11 @@ export const QueriesDebug = observer((props:{
   const classes = useStyles();
   const [selected, setSelected] = useState<GraphQLStore|undefined>(/*queries&&queries.length > 0 ? queries[0] : undefined*/);
   const [graphiQL, setGraphiQL] = useState('');
+  const [variablesStr, setVariablesStr] = useState('');
 
   useEffect(()=>{
     try{
+      setVariablesStr(JSON.stringify(selected?.variables||{}));
       setGraphiQL(selected?.gql ? print(parse(selected?.gql)) : '');
     }
     catch(e){
@@ -54,17 +76,26 @@ export const QueriesDebug = observer((props:{
         <GQLList gqls = {queries} selected = {selected} onSelect={(gql)=>{setSelected(gql)}} />
       }
       <div className={classes.container}>      
-        <Grid container spacing={1} >
+        <Grid container spacing={1} alignItems="stretch" >
           <Grid item md={4} className={classes.editorSchell}>
             <Typography className={classes.titleText} variant="h6">GraphiQL</Typography>
             <CodeMirrorEditor value = {graphiQL} mode="graphql" onChange = {value=>setGraphiQL(value)}/>
           </Grid>
-          <Grid item md={4}>
-          <Typography variant="h6" className={classes.titleText}>Variables</Typography>
-          ddd
+          <Grid item md={4} className={classes.editorSchell}>
+            <Typography variant="h6" className={classes.titleText}>Variables</Typography>
+            <CodeMirrorEditor value = {variablesStr} mode="application/json" onChange = {value=>setVariablesStr(value)}/>
           </Grid>
-          <Grid item md={4}>
-            3
+          <Grid item md={4} className={classes.editorSchell}>
+            <Typography variant="h6" className={classes.titleText}>{intl.get('result')}</Typography>
+            <div className={classes.error}>
+              <div className={classes.errorInner}> 
+                <pre className= {classes.pre}>error error error error error error error1 
+                error error error error error 2
+                error error error error 3
+                error error error error error error error error error error error error error error error error 
+                </pre>
+              </div>
+            </div>
           </Grid>
         </Grid>
         </div>
