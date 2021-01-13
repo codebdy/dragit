b2a7b641646eb26306593c720afd6c21c0a22cd4
Water.Li <lizongbin@hotmail.com>
2021-01-13 21:29:32 +0800

    Delete IQueryParam.ts

--- src/Components/ListView/IQueryParam.ts
+++
@@ -1,9 +0,0 @@
-import { IFieldOrder } from "./IFieldOrder";
-
-export interface IQueryParam{
-  keywords?: string,
-  filterValues?: Array<string>,
-  orders?: Array<IFieldOrder>,
-  first: number,
-  page:number,//第几页
-}
