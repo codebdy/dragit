da35218b2d4f47705a2ccb48e97c10470e465a24
Water.Li <lizongbin@hotmail.com>
2021-01-15 11:15:52 +0800

    Update ModelSelector.tsx

--- src/AdminBoard/Workspace/Page/Debug/ModelSelector.tsx
+++ src/AdminBoard/Workspace/Page/Debug/ModelSelector.tsx
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useAppStore } from 'Store/Helpers/useAppStore';
 import {observer} from 'mobx-react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { useEffect } from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,9 +17,50 @@ const useStyles = makeStyles((theme: Theme) =>
 export const ModelSelector = observer(() => {
   const classes = useStyles();
   const appStore = useAppStore();
+  const [rect, setRect] = useState<DOMRect>();
 
-  const selectedDom = document.querySelector(`[data-rxid="${appStore.selectModelComponentRxid}"]`);
-  const rect = selectedDom?.getBoundingClientRect();
+  const getDom = ()=>{
+    return document.querySelector(`[data-rxid="${appStore.selectModelComponentRxid}"]`);
+  }
+
+  const getDomRect = ()=>{
+    const selectedDom = getDom();
+    return selectedDom?.getBoundingClientRect();
+  }
+
+  const reComputeRect = ()=>{
+    setRect(getDomRect());
+  }  
+  
+  const handleScroll = ()=>{
+    console.log('Scroll')
+    reComputeRect();
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
+
+  useEffect(()=>{
+    const currentRect = getDomRect();
+    //滚动条高度+视窗高度 = 可见区域底部高度
+    const visibleBottom = window.scrollY + document.documentElement.clientHeight;
+    //可见区域顶部高度
+    const visibleTop = window.scrollY;
+
+    if(currentRect && (currentRect.bottom <= visibleTop || currentRect.top >= visibleBottom)){
+      //document.documentElement.scrollTop = currentRect.top;
+      getDom()?.scrollIntoView()
+    }
+
+    reComputeRect();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [appStore.selectModelComponentRxid])
 
   return (
     appStore.selectModelComponentRxid && rect
