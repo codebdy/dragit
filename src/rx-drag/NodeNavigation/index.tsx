e4c209aa9250566d064dbcd6c0ec5cf6e58abd79
Water.Li <lizongbin@hotmail.com>
2021-01-28 10:16:35 +0800

    finished

--- src/rx-drag/NodeNavigation/index.tsx
+++ src/rx-drag/NodeNavigation/index.tsx
@@ -3,12 +3,42 @@ import { observer } from 'mobx-react';
 import { useRxDragShellStore } from 'rx-drag/store/useRxDragShellStore';
 import './style.css';
 import { NodeNavigationLabel } from './NodeNavigationLabel';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useRxDragStore } from 'rx-drag/store/useDesign';
+import { IRxMeta } from 'rx-drag/IRxMeta';
+import { RxNode } from 'rx-drag/RxNode';
 
 
 export const NodeNavigation = observer(() => {
   const shellStore = useRxDragShellStore();
+  const [labels, setLabels] = useState<Array<{node:RxNode<IRxMeta>, className?:string}>>([]);
   const elRef = useRef<HTMLDivElement>(null);
+  const rxDragStore = useRxDragStore();
+  const creatNavLabels = ()=>{
+    var newLabels:Array<{node:RxNode<IRxMeta>, className?:string}> = [];
+    var node = rxDragStore?.selectedNode;
+    var interNode:RxNode<IRxMeta>|undefined = undefined;
+    while(node && node?.id !== rxDragStore?.canvas?.id){
+      if(node.id === rxDragStore?.selectedNode?.id){
+        newLabels.unshift({node, className:'checked'})
+        interNode = node.parent;
+      }
+      else if(interNode?.id === node.id){
+        newLabels.unshift({node, className:'inter'})
+      }
+      else{
+        newLabels.unshift({node})
+      }
+      node = node.parent;
+    }
+    setLabels(newLabels);
+  }
+
+  useEffect(()=>{
+    creatNavLabels();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[rxDragStore?.selectedNode])
+
   useEffect(()=>{
     const dom = elRef?.current;
     if(dom){
@@ -21,19 +51,22 @@ export const NodeNavigation = observer(() => {
       ref = {elRef}
       style = {
         {
-          borderColor:shellStore?.themeOptions.borderColor,
+          borderColor: shellStore?.themeOptions.borderColor,
         }
       }
     >
-        <NodeNavigationLabel>
-          div
-        </NodeNavigationLabel>
-        <NodeNavigationLabel className = 'inter'>
-          Column
-        </NodeNavigationLabel>
-        <NodeNavigationLabel className = 'checked'>
-          GridItem
-        </NodeNavigationLabel>
+      {
+        labels.map((label, index)=>{
+          return(
+            <NodeNavigationLabel 
+              key = {index} className = {label.className}
+              onClick = {()=>{rxDragStore?.setSelectedNode(label.node)}}
+            >
+              {label.node?.meta?.name}
+            </NodeNavigationLabel>
+          )
+        })
+      }
     </div>
   );
 })
