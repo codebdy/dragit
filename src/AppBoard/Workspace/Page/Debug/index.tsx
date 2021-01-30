f6b05c7c464b72852fe393fec9bc2753ba5393e1
Water.Li <lizongbin@hotmail.com>
2021-01-30 17:19:47 +0800

    change debug button to popup menu

--- src/AppBoard/Workspace/Page/Debug/index.tsx
+++ src/AppBoard/Workspace/Page/Debug/index.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import { makeStyles, createStyles, Theme, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
-import SpeedDial from '@material-ui/lab/SpeedDial';
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
 import MdiIcon from 'Components/Common/MdiIcon';
 import intl from 'react-intl-universal';
-import { SpeedDialIcon } from '@material-ui/lab';
 import GraphQLDebug from './DebugGraphQL';
 import { useLeftDrawer } from 'Store/Helpers/useDragItStore';
 import { useThemeSettings } from "AppBoard/store/useThemeSettings";
 import {observer} from 'mobx-react';
 import { DARK } from 'AppBoard/store/ThemeSettings';
 import { DebugModelTree } from './DebugModelTree';
-import { Hidden } from '@material-ui/core';
+import { Fab, Hidden,  Menu, MenuItem } from '@material-ui/core';
 import { DebugStore, DebugStoreProvider } from './DebugStore';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    speedDial: {
+    debugButton: {
       position: 'fixed',
-      bottom: 0,
+      bottom: '4px',
       transition: 'left 0.3s',
+      width:'40px',
+      height:'40px',
+    },
+    menuItem:{
+      padding:theme.spacing(1, 3),
     },
   }),
 );
 
 export const Debug = observer(()=>{
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [gqlOpen, setGqlOpen] = React.useState(false);
   const [treeOpen, setTreeOpen] = React.useState(false);
   const leftDrawer = useLeftDrawer();
   const fabLeft = leftDrawer.isMini ? leftDrawer.compactWidth : leftDrawer.fullWidth;
   const themeSettings = useThemeSettings();
+  const isMenuOpen = Boolean(anchorEl);
+
   const theme = responsiveFontSizes(createMuiTheme({
     palette: {
       type: DARK,
@@ -45,16 +49,17 @@ export const Debug = observer(()=>{
     
   }));
 
-  const handleOpen = () => {
-    setOpen(true);
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
+
   const handleClose = ()=>{
-    setOpen(false);
+    setAnchorEl(null);
   }
 
   const handleOpentree = () => {
-    setOpen(false);
+    setAnchorEl(null);
     setTreeOpen(true);
   };
 
@@ -63,42 +68,43 @@ export const Debug = observer(()=>{
   }
 
   const handleOpenGql = ()=>{
-    setOpen(false);
+    setAnchorEl(null);
     setGqlOpen(true);
   }
   
   return (
     <Hidden smDown>
       <DebugStoreProvider value = {new DebugStore()}>
-        <SpeedDial
-          ariaLabel="Debug SpeedDial"
-          className={classes.speedDial}
-          icon={
-            <SpeedDialIcon 
-              icon={<MdiIcon iconClass = "mdi-android-debug-bridge"/>}
-              openIcon={<MdiIcon iconClass = "mdi-close"/>}
-            />
-          }
-          hidden = {treeOpen || gqlOpen}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          open={open}
-          direction='up'
-          style={{left:(fabLeft) + 'px'}}
+        <Fab 
+          className = {classes.debugButton} 
+          style={{left:(fabLeft + 4) + 'px'}}
+          color = "primary"
+          onClick = {handleOpen}
         >
-          <SpeedDialAction
-            icon={<MdiIcon iconClass = "mdi-graphql" />}
-            tooltipTitle={'GraphQL ' + intl.get('debug')}
-            tooltipPlacement = "right"
-            onClick={(handleOpenGql)}
-          />
-          <SpeedDialAction
-            icon={<MdiIcon iconClass = "mdi-file-tree" />}
-            tooltipTitle={'Model Tree ' + intl.get('debug')}
-            tooltipPlacement = "right"
-            onClick={handleOpentree}
-          />
-        </SpeedDial>
+          <MdiIcon iconClass = "mdi-android-debug-bridge" size={20}/>
+        </Fab>
+        <Menu
+          anchorEl={anchorEl}
+          getContentAnchorEl={null}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={isMenuOpen}
+          onClose={handleClose}
+          
+        >
+          <MenuItem onClick={handleOpenGql} className = {classes.menuItem}>
+            {'GraphQL ' + intl.get('debug')} 
+          </MenuItem>
+          <MenuItem onClick={handleOpentree} className = {classes.menuItem}>
+            {'Model Tree ' + intl.get('debug')} 
+          </MenuItem>
+        </Menu>
         <ThemeProvider theme={theme}>
           <GraphQLDebug open={gqlOpen} onClose = {()=>setGqlOpen(false)} />
           <DebugModelTree open={treeOpen} onClose = {handleCloseTree} />
