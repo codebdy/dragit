75fc0c379e604c9ba6d1b53829b507a8eb9c3718
Water.Li <lizongbin@hotmail.com>
2021-02-02 21:34:01 +0800

    do some configs

--- src/Components/ListView/ListViewBatchActions/ListViewBatchActionsConfig.ts
+++ src/Components/ListView/ListViewBatchActions/ListViewBatchActionsConfig.ts
@@ -1,15 +1,20 @@
 import { MetaConfig } from "Base/RXNode/MetaConfig";
 import { IPropConfig } from "rx-drag/models/IPropConfig";
 import { IMeta } from "Base/RXNode/IMeta";
+import marginConfigs from "Components/common/configs/marginConfigs";
 
 export class ListViewBatchActionsConfig extends MetaConfig{
   
   accept(child:IMeta){
+    if(child.name === 'Button'||child.name === 'IconButton'){
+      return true;
+    }
     return false;
   }
 
   getPropConfigs(): Array<IPropConfig>{
     return [
+      ...marginConfigs,
     ]
   }
 
--- src/Components/ListView/ListViewFilters/ListViewFiltersConfig.ts
+++ src/Components/ListView/ListViewFilters/ListViewFiltersConfig.ts
@@ -1,15 +1,20 @@
 import { MetaConfig } from "Base/RXNode/MetaConfig";
 import { IPropConfig } from "rx-drag/models/IPropConfig";
 import { IMeta } from "Base/RXNode/IMeta";
+import marginConfigs from "Components/common/configs/marginConfigs";
 
 export class ListViewFiltersConfig extends MetaConfig{
   
   accept(child:IMeta){
+    if(child.name === 'ListViewKeywordFilter' || child.name === 'ListViewEnumFilter' || child.name === 'ListViewRangeFilter'){
+      return true;
+    }
     return false;
   }
 
   getPropConfigs(): Array<IPropConfig>{
     return [
+      ...marginConfigs,
     ]
   }
 
