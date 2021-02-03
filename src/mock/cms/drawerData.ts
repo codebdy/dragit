f2c3ddba5586c0707050ee71f2dab75fadc81e71
Water.Li <lizongbin@hotmail.com>
2021-02-03 18:16:22 +0800

    Update drawerData.ts

--- src/mock/cms/drawerData.ts
+++ src/mock/cms/drawerData.ts
@@ -2,7 +2,6 @@ export var cmsDrawerData= [
   {
     type: 'item',
     title: '分析看板',
-    moduleSlug:'dashboard',
     icon: 'mdi-view-dashboard',
     chip: {
         color: 'primary',
@@ -18,98 +17,113 @@ export var cmsDrawerData= [
     icon: 'mdi-email-outline',
 
     children: [
-        {
-            title:"询盘",
-            type:'item',
-            icon: 'mdi-circle-small',
-            to: '/admin/module/enquery/',
-            badge: {
-                color: 'secondary',
-                field: 'inquiries',
-                size: 'small',
-            }               
-        },
-        {
-            title:"垃圾箱",
-            type:'item',
-            icon: 'mdi-circle-small',
-            //to: '/admin/module/role/',
-        },
-        {
-            title:"拦截设置",
-            type:'item',
-            icon: 'mdi-circle-small',
-            //to: '/admin/module/role/',
-        }
-
-
+      {
+          title:"询盘",
+          type:'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/enquery/',
+          badge: {
+              color: 'secondary',
+              field: 'inquiries',
+              size: 'small',
+          }               
+      },
+      {
+          title:"垃圾箱",
+          type:'item',
+          icon: 'mdi-circle-small',
+          //to: '/admin/module/role/',
+      },
+      {
+          title:"拦截设置",
+          type:'item',
+          icon: 'mdi-circle-small',
+          //to: '/admin/module/role/',
+      }
+    ]
+  },
+  {
+    type: 'item',
+    title: '媒体库',
+    icon: 'mdi-image-auto-adjust',
+    auths:[]
+  },  
+  {
+    title: '文章管理',
+    type: 'group',
+    icon: 'mdi-text-box-outline',
+    children: [
+      {
+          title: '文章',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/article/',
+      },
+      {
+          title: '频道',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/article-chanel',
+      },
+      {
+          title: '标签',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/article-tag',
+      },
+      {
+          title: '属性',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/article-attribute',
+      },
+    ],
+  },
+  {
+    title: '产品管理',
+    type: 'group',
+    icon: 'mdi-basket-outline',
+    children:[
+      {
+          title: '产品',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/product',
+      },
+      {
+          title: '分类',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/product-category',
+      },
+      {
+          title: '属性',
+          type: 'item',
+          icon: 'mdi-circle-small',
+          to: '/admin/module/product-attribute',
+      },
     ]
 
   },
 
   {
     type: 'item',
-    title: '媒体库',
-    moduleSlug: 'medias',
-    icon: 'mdi-image-auto-adjust',
+    title: '页面管理',
+    icon: 'mdi-file-outline',
     auths:[]
   },
-  {
-      title: '文章管理',
-      type: 'group',
-      icon: 'mdi-text-box-outline',
-      children: [
-          {
-              title: '文章',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/article/',
-          },
-          {
-              title: '频道',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/article-chanel',
-          },
-          {
-              title: '标签',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/article-tag',
-          },
-          {
-              title: '属性',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/article-attribute',
-          },
-      ],
-  },
-  {
-      title: '产品管理',
-      type: 'group',
-      icon: 'mdi-basket-outline',
-      children:[
-          {
-              title: '产品',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/product',
-          },
-          {
-              title: '分类',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/product-category',
-          },
-          {
-              title: '属性',
-              type: 'item',
-              icon: 'mdi-circle-small',
-              to: '/admin/module/product-attribute',
-          },
-      ]
 
+  {
+    type: 'item',
+    title: '网站导航',
+    icon: 'mdi-sitemap',
+    auths:[]
   },
 
+  {
+    type: 'item',
+    title: '网站设置',
+    icon: 'mdi-application-cog',
+    auths:[]
+  },
 ]
