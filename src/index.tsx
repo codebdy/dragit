2dde028a7eeb09981c431a41c1a514738afc5e1a
Water.Li <11011968@qq.com>
2021-04-08 12:05:43 +0800

    Update index.tsx
    remove serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from '@apollo/react-hooks';
 import client from './mock/client';
 import { CssBaseline } from '@material-ui/core';
@@ -26,4 +26,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
