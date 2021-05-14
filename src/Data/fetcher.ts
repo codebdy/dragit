de881b46248b27287d54bd1a73117d60397e2542
Water.Li <lizongbin@hotmail.com>
2021-05-15 00:16:29 +0800

    Update fetcher.ts

--- src/Data/fetcher.ts
+++ src/Data/fetcher.ts
@@ -1,18 +1,20 @@
+import { TOKEN_NAME } from "Utils/consts";
+
 const serverUrl = "http://127.0.0.1:8000/api";
 const fetcher = async (url:string) => {
+  const token = localStorage.getItem(TOKEN_NAME);
   const res = await fetch(
       serverUrl + url,
       {
         method: 'GET',
         headers:{
           'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          authorization: token ? `Bearer ${token}` : "",
         }
       }
     )
 
-  // 如果状态码不在 200-299 的范围内，
-  // 我们仍然尝试解析并抛出它。
   if (!res.ok) {
     const error = new Error('An error occurred while fetching the data.') as any;
     // 将额外的信息附加到错误对象上。
