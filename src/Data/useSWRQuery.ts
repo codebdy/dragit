f60ce1d1c5b5bbb803c0007fd1696a0569070260
Water.Li <lizongbin@hotmail.com>
2021-06-17 16:21:15 +0800

    can show useMagicQueryInfinite error

--- src/Data/useSWRQuery.ts
+++ src/Data/useSWRQuery.ts
@@ -3,17 +3,16 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import useSWR, { SWRResponse } from "swr";
 import { LOGIN_URL } from "Utils/consts";
-import { DataError } from "./DataError";
 import { fetcher } from "./fetcher";
 
-export function useSWRQuery<T>(api:AxiosRequestConfig):SWRResponse<T, DataError>&{loading?:boolean}{
+export function useSWRQuery<T>(api:AxiosRequestConfig):SWRResponse<T, any>&{loading?:boolean}{
   const history = useHistory();
-  const rtValue = useSWR<T, DataError>(api.url||null, fetcher);
+  const rtValue = useSWR<T>(api.url||null, fetcher);
   useEffect(()=>{
     if(rtValue?.error?.status === 401){
       history?.push(LOGIN_URL);
     }
   },[rtValue.error, history, rtValue])
-
-  return {...rtValue, loading: !rtValue.data && !rtValue.error};
+  const rtError = rtValue.error ? {message:rtValue.error?.message?.error} : undefined;
+  return {...rtValue, loading: !rtValue.data && !rtValue.error, error:rtError};
 }
