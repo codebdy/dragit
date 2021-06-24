b975b05071af698d2f63be3cd0a9091be76f5a52
Water.Li <lizongbin@hotmail.com>
2021-06-24 09:36:38 +0800

    Update index.tsx

--- src/Components/Inputs/TextBox/index.tsx
+++ src/Components/Inputs/TextBox/index.tsx
@@ -1,18 +1,28 @@
 import { TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { stringValue } from 'rx-drag/utils/stringValue';
 
 export const TextBox = React.forwardRef((props:any, ref:any)=>{
   const {value, shrinkLabel, type, onChange, ...rest} = props;
+  const [inputValue, setInputValue] = useState(value);
+
+  useEffect(()=>{
+    setInputValue(value);
+  },[value])
+
+  const handleBlur = ()=>{
+    if(type === 'number'){
+      onChange && onChange({target:{value: inputValue ? parseFloat(inputValue) : undefined}})
+    }
+    else{
+      onChange && onChange({target:{value: inputValue}})
+    }
+  }
 
   const handleOnchange = (event:React.ChangeEvent<HTMLInputElement>)=>{
     let newValue = event.target.value as string;
-    if(type === 'number'){
-      onChange && onChange({target:{value: newValue ? parseFloat(newValue) : undefined}})
-    }
-    else{
-      onChange && onChange(event)
-    }
+    setInputValue(newValue);
+
   }
 
   return (
@@ -20,7 +30,8 @@ export const TextBox = React.forwardRef((props:any, ref:any)=>{
       ref={ref}
       type = {type}
       onChange = {handleOnchange}
-      value = {stringValue(value)}
+      onBlur = {handleBlur}
+      value = {stringValue(inputValue)}
       {...rest} 
       InputLabelProps = {
         shrinkLabel ? 
